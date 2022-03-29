63e3405200eb0606d5a4f908617a6795286ecc3b
Emma <lol@example.com>
2022-03-29 17:41:02 +0300

    countdown1

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const countdown = document.querySelector('.countdown');
+
+//launch date
+
+const launchD = new Date('June 1, 2022 13:00:00').getTime;
+
+//update every second
+
+const intvl =setInterval(() => {
+//get todays date ms
+    const now = new Date.getTime();
+
+    //distance from now to launch date
+    const distance = launchD - now;
+
+    console.log(distance);
+
+    //time calculation
+    const days = Math.floor(distance/(1000*60*60*24));
+    const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+    const mins = Math.floor((distance % (1000*60*60))/(1000*60));
+    const second = Math.floor((distance % (1000*60))/(1000));
+
+    // display result
+    countdown.innerHTML=`
+        <div>${days}<span>Days</span></div>
+        <div>${hours}<span>Hours</span></div>
+        <div>${mins}<span>Minutes</span></div>
+        <div>${second}<span>Seconds</span></div>
+    `;
+
+    //if launch date passed
+    if(distance<0)
+    {
+        clearInterval(intvl);
+        countdown.style.color = '#17a2b8';
+        countdown.innerHTML='Launched!';
+    }
+}, 1000);
